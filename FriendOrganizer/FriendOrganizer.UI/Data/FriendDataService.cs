11a29fa728e53ca2cd11703524798330afdfdde7
ErikJosander <josander.erik@gmail.com>
2020-04-30 11:06:58 +0200

    Added New Project
    Models, Data And MWWP Views Added

---
+++ FriendOrganizer/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using FriendOrganizer.Models;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: Load from real database
+            yield return new Friend { FirstName = "Alexander", LastName = "Johansson" };
+            yield return new Friend { FirstName = "Jamie", LastName = "Wilton" };
+            yield return new Friend { FirstName = "Robin", LastName = "Johansson" };
+            yield return new Friend { FirstName = "Caj", LastName = "Rydholm" };
+        }
+    }
+}
---
+++ FriendOrganizer/FriendOrganizer.UI/Data/IFriendDataService.cs
@@ -0,0 +1,10 @@
+﻿using FriendOrganizer.Models;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public interface IFriendDataService
+    {
+        IEnumerable<Friend> GetAll();
+    }
+}
